597c296cc9dd8148486cbad58812917a5a9c3f04
ITM-RAJA <108492373+ITM-RAJA@users.noreply.github.com>
2022-07-10 22:53:42 -0700

    Update script.js
    coder js chenge

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 // Typing Script JS
 var typed = new Typed(".typing",{
-    strings: ["CODER", "", "Web Developer", ""],
+    strings: ["CODER", "", "", "Web Developer"],
     typeSpeed: 80,
     backSpeed: 80,
 });
 
 var typed = new Typed(".typing-2",{
-    strings: ["Web Developer", "", "", "Programmer"],
+    strings: ["Web Developer", "I love to code", "", "Programmer"],
     typeSpeed: 100,
     backSpeed: 60,
 });
